3eb33a25729d02739a709522ffb9faf8b2a0cd73
Zachnad0 <zjhobbs@gmail.com>
2023-12-20 19:27:57 +1300

    Day 14 part 1 complete. TODO is a whole lot more of them damn bruh ain't no way.

---
+++ CSharp/Day14.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+	public class Day14 : IDailyProgram
+	{
+		private const char MVBL_CHAR = 'O', STTC_CHAR = '#', GRND_CHAR = '.';
+		private const int CYCLE_COUNT = 1000000000;
+
+		public static void RunP1(string[] args)
+		{
+			string[] lines = File.ReadAllLines("../input14.txt");
+			// Remove if not matrix:
+			char[][] rocksMatrix = lines.ToMatrix();
+
+			// Move all rocks as far negative Y as possible.
+			int mWidth = rocksMatrix.Length, mHeight = rocksMatrix[0].Length;
+
+			for (int y = 1; y < mHeight; y++) // Start at bottom, and pull down except first
+			{
+				for (int x = 0; x < mWidth; x++)
+				{
+					if (rocksMatrix[x][y] == MVBL_CHAR) // If a movable rock, move as far forward as possible
+					{
+						// Check positions below, relocate to next available pos
+						for (int yOffsetPos = y - 1; yOffsetPos >= 0; yOffsetPos--)
+						{
+							if (rocksMatrix[x][yOffsetPos] is MVBL_CHAR or STTC_CHAR) // If cannot go further, put it just one above
+							{
+								rocksMatrix[x][y] = GRND_CHAR;
+								rocksMatrix[x][yOffsetPos + 1] = MVBL_CHAR;
+								break;
+							}
+							else if (yOffsetPos == 0)
+							{
+								rocksMatrix[x][y] = GRND_CHAR;
+								rocksMatrix[x][yOffsetPos] = MVBL_CHAR;
+								break;
+							}
+						}
+					}
+				}
+			}
+
+			// Calculate load
+			long totalLoad = 0;
+			for (int invY = 1; invY <= mHeight; invY++)
+				totalLoad += rocksMatrix.Count(col => col[^invY] == MVBL_CHAR) * invY;
+
+			Console.WriteLine(rocksMatrix.ToString(""));
+			Console.WriteLine($"\n\nTotal Load: {totalLoad}");
+		}
+
+		public static void RunP2(string[] args)
+		{
+			string[] lines = File.ReadAllLines("../input14.txt");
+			// Remove if not matrix:
+			char[][] rocksMatrix = lines.ToMatrix();
+			//DateTime startTime = DateTime.Now;
+
+			List<char[][]> prevMatrices = new();
+			for (int cycleNum = 0; cycleNum < CYCLE_COUNT; cycleNum++)
+			{
+				if (cycleNum % 1000000 == 0)
+					Console.WriteLine($"Beginning {cycleNum / 1000000}-millionth cycle.");
+				else if (cycleNum % 10000 == 0)
+					Console.WriteLine($"Beginning {cycleNum / 1000}-thousandth cycle.");
+
+				for (int spinCycleProgress = 0; spinCycleProgress < 4; spinCycleProgress++)
+				{
+					// Move all rocks as far in current dir as possible
+					int mWidth = rocksMatrix.Length, mHeight = rocksMatrix[0].Length;
+					for (int y = 1; y < mHeight; y++) // Start at bottom, and pull down except first
+					{
+						Parallel.For(0, mWidth, x =>
+						{
+							if (rocksMatrix[x][y] == MVBL_CHAR) // If a movable rock, move as far forward as possible
+							{
+								// Check positions below, relocate to next available pos
+								for (int yOffsetPos = y - 1; yOffsetPos >= 0; yOffsetPos--)
+								{
+									if (rocksMatrix[x][yOffsetPos] is MVBL_CHAR or STTC_CHAR) // If cannot go further, put it just one above
+									{
+										rocksMatrix[x][y] = GRND_CHAR;
+										rocksMatrix[x][yOffsetPos + 1] = MVBL_CHAR;
+										break;
+									}
+									else if (yOffsetPos == 0)
+									{
+										rocksMatrix[x][y] = GRND_CHAR;
+										rocksMatrix[x][yOffsetPos] = MVBL_CHAR;
+										break;
+									}
+								}
+							}
+						});
+					}
+
+					// Rotate matrix 90 degrees clockwise
+					rocksMatrix = rocksMatrix.RotateMatrix();
+				}
+
+				bool isAlreadyIncl = false;
+				Parallel.ForEach(prevMatrices, (m, ls) => // TODO Day 14 Part 2 maybe LCM of each rock's mini cycle???
+				{
+					if (rocksMatrix.Identical(m))
+					{
+						isAlreadyIncl = true;
+						ls.Stop();
+					}
+				});
+
+				if (isAlreadyIncl)
+					break;
+				prevMatrices.Add(rocksMatrix);
+			}
+
+			//long ts = (long)(DateTime.Now - startTime).TotalSeconds;
+			//Console.WriteLine($"{CYCLE_COUNT} cycles took {ts} seconds, therefore it would take {1000000000 / CYCLE_COUNT * ts} seconds to do full.");
+
+			// Calculate load
+			long totalLoad = 0;
+			for (int invY = 1; invY <= rocksMatrix[0].Length; invY++)
+				totalLoad += rocksMatrix.Count(col => col[^invY] == MVBL_CHAR) * invY;
+
+			Console.WriteLine(rocksMatrix.ToString<char>(""));
+			Console.WriteLine($"\n\nTotal Load: {totalLoad}");
+		}
+	}
+}
--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -32,17 +32,17 @@ namespace CSharp
 			//Day12.RunP1(args);
 			//Day12.RunP2(args);
 			//Day13.RunP1(args);
-			//Day13.RunP2(args); // TODO Days 14 up to and including 18
+			//Day13.RunP2(args);
+			//Day14.RunP1(args);
+			Day14.RunP2(args);
 
-			Day19.RunP1(args);
 
 			Console.ReadKey();
 		}
 	}
 
-	public static class UsefulAoCUtils
+	public static class UsefulAoCUtils // TODO add these to ZUtilLib
 	{
-		// TODO add to ZUtilLib
 		public static char[][] ToMatrix(this string[] lines)
 		{
 			int mWidth = lines[0].Length, mHeight = lines.Length;
@@ -57,6 +57,61 @@ namespace CSharp
 			}
 
 			return outMatrix;
+		}
+
+		public static string ToString<T>(this T[][] matrix, string horizSeperator = "\t", string vertSeperator = "\n")
+		{
+			string outputString = "";
+			int mWidth = matrix.Length, mHeight = matrix[0].Length; ;
+			for (int y = 0; y < mHeight; y++)
+			{
+				for (int x = 0; x < mWidth; x++)
+					outputString += $"{(x != 0 ? horizSeperator : "")}{matrix[x][y]}";
+				outputString += y != mWidth - 1 ? vertSeperator : "";
+			}
+
+			return outputString;
+		}
+
+		/// <summary>
+		/// Rotates the matrix 90 degrees if <paramref name="clockwise"/>, otherwise counter-clockwise.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="clockwise"></param>
+		/// <returns>A new array containing the elements in rotated position</returns>
+		public static T[][] RotateMatrix<T>(this T[][] matrix, bool clockwise = true)
+		{
+			int origMHeight = matrix[0].Length, origMWidth = matrix.Length;
+			T[][] outMatrix = new T[origMHeight][];
+			for (int i = 0; i < origMHeight; i++)
+				outMatrix[i] = new T[origMWidth];
+
+			for (int origMY = 0; origMY < origMHeight; origMY++)
+			{
+				for (int origMX = 0; origMX < origMWidth; origMX++)
+				{
+					if (clockwise)
+						outMatrix[origMHeight - 1 - origMY][origMX] = matrix[origMX][origMY];
+					else
+						outMatrix[origMY][origMWidth - 1 - origMX] = matrix[origMX][origMY];
+				}
+			}
+
+			return outMatrix;
+		}
+
+		public static bool Identical<T>(this T[][] matrix, T[][] otherMatrix)
+		{
+			if (matrix is null || otherMatrix is null || matrix.Length != otherMatrix.Length || matrix[0].Length != otherMatrix[0].Length)
+				return false;
+
+			bool equivelant = true; // If the previous matrix is the same as this matrix, then we must be in a perpetually looping loop.
+			for (int y = 0; y < matrix[0].Length && equivelant; y++)
+				for (int x = 0; x < matrix.Length && equivelant; x++)
+					if (!matrix[x][y].Equals(otherMatrix[x][y]))
+						equivelant = false;
+
+			return equivelant;
 		}
 	}
 }
